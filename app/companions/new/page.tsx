24224fd2cfef347a7aac0dd5a401ec9a9387c20a
Diya Patel <diya19p@gmail.com>
2025-09-21 20:42:27 -0400

    Initialize Companion Card, Form, List w/ Homepage

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import CompanionForm from '@/components/CompanionForm';
 
 const NewCompanion = () => {
-	return <div>New Companion</div>;
+	return (
+		<>
+			<main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+				<article className="w-full gap-4 flex flex-col">
+					<h1>Companion Builder</h1>
+
+					<CompanionForm />
+				</article>
+			</main>
+		</>
+	);
 };
 
 export default NewCompanion;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
+import { recentSessions } from '@/constants';
 
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+	return (
+		<>
+			<main>
+				<h1>Popular Companions</h1>
+				<section className="home-section">
+					<CompanionCard
+						id="1"
+						name="Companion 1"
+						topic="Topic 1"
+						subject="Subject 1"
+						duration={30}
+						color="#ffda6e"
+						bookmarked={false}
+					/>
+					<CompanionCard
+						id="1"
+						name="Companion 1"
+						topic="Topic 1"
+						subject="Subject 1"
+						duration={30}
+						color="#ffda6e"
+						bookmarked={false}
+					/>
+					<CompanionCard
+						id="1"
+						name="Companion 1"
+						topic="Topic 1"
+						subject="Subject 1"
+						duration={30}
+						color="#ffda6e"
+						bookmarked={false}
+					/>
+				</section>
+				<section className="home-section">
+					<CompanionsList
+						title="Your Companions"
+						companions={recentSessions}
+						classNames="w-[2/3] max-lg:w-full"
+					/>
+					<CTA />
+				</section>
+			</main>
+		</>
+	);
+};
 
-export default Page
+export default Page;
